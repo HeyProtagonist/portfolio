678170c615fecf30fe7ab85ff7ed6b7f17954f55
Protagonist <75655740+HeyProtagonist@users.noreply.github.com>
2023-04-21 07:06:43 +0000

    vercel analytics initialize

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Analytics } from '@vercel/analytics/react'
 
 const APP = (
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 )
 
