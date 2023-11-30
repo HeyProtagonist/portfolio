81f00a81aac7a5e1cef90274231e715327705a7c
anguram <anguram.shanmugam@invisinet.com>
2023-11-30 19:33:12 +0530

    Refresh

--- src/components/atomic/organisms/Social.tsx
+++ src/components/atomic/organisms/Social.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { VscGithub } from 'react-icons/vsc'
-import { SiTwitter } from 'react-icons/si'
+import { VscGithub, VscMail } from 'react-icons/vsc'
+import { SiMailDotRu, SiMailchimp, SiTwitter } from 'react-icons/si'
+import Avatar from '../../../avatar.jpg'
 
 interface Props {}
 
@@ -11,25 +12,37 @@ export const Social = (props: Props) => {
     <div className="flex items-center justify-between w-11/12 md:justify-start md:space-x-10 h-20">
       <img
         className="text-center w-14 h-14 rounded-full overflow-hidden bg-white"
-        src={`https://pbs.twimg.com/profile_images/1600003925343862784/HRLB3eW5_400x400.jpg`}
+        src={Avatar}
         alt="Anguram Shanmugam With A Cool Posture 😁"
       />
 
       <a
         href={`https://github.com/${userName}`}
-        className="font-semibold flex items-center justify-between h-14 border-2 border-transparent hover:border-gray-400 px-4 space-x-3 rounded-full"
+        className="font-semibold flex items-center justify-between h-14 w-14 border-2 border-transparent hover:border-gray-400 px-4 space-x-1 rounded-full"
       >
         <VscGithub className="w-6 h-6 object-contain" />
-        <p>Github</p>
       </a>
 
       <a
         href={`https://twitter.com/${userName}`}
-        className="font-semibold flex items-center justify-between h-14 border-2 border-transparent hover:border-gray-400 px-4 space-x-3 rounded-full"
+        className="font-semibold flex items-center justify-between h-14 w-14 border-2 border-transparent hover:border-gray-400 px-4 space-x-1 rounded-full"
       >
         <SiTwitter className="w-6 h-6 object-contain" />
-        <p>Twitter</p>
       </a>
+
+      <a
+        href={`mailto:ThisIsAnguram@gmail.com`}
+        className="font-semibold flex items-center justify-between h-14 w-14 border-2 border-transparent hover:border-gray-400 px-4 space-x-1 rounded-full"
+      >
+        <SiMailDotRu className="w-6 h-6 object-contain" />
+      </a>
+
+      {/* <a
+        href={`mailto:ThisIsAnguram@gmail.com`}
+        className="font-semibold flex items-center justify-between h-14 border-2 border-transparent hover:border-gray-400 px-4 space-x-1 rounded-full"
+      >
+        <SiTwitter className="w-6 h-6 object-contain" />
+      </a> */}
     </div>
   )
 }
